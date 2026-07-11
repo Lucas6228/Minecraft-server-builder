7f4770d1650f71e2fb16694697f24117b570537e
Lucas6228 <146557843+Lucas6228@users.noreply.github.com>
2026-07-11 16:11:08 -0400

    Update installpaper(deb).sh to 26.1.2

--- installpaper(deb).sh
+++ installpaper(deb).sh
@@ -5,10 +5,10 @@ echo Installing curl
 sudo apt install -y curl
 sudo apt install -y openjdk-21-jdk
 echo Making folder for server
-mkdir PaperMC1.21.10
-cd PaperMC1.21.10
+mkdir PaperMC26.1.2
+cd PaperMC26.1.2
 echo Downloading server jar
-curl -o server.jar https://fill-data.papermc.io/v1/objects/a61a0585e203688f606ca3a649760b8ba71efca01a4af7687db5e41408ee27aa/paper-1.21.10-117.jar
+curl -o server.jar https://fill-data.papermc.io/v1/objects/1d70b1dab9cf4a6de615209a536f3a45a2186240253c428213ce2188ab95e5f7/paper-26.1.2-74.jar
 echo Starting server for the first time to create files. If it errors. Ignore it
 java -Xms1G -Xmx1G -jar server.jar
 echo Accepting EULA
@@ -18,12 +18,12 @@ sed -i 's/eula=false/eula=true/' eula.txt
 echo Adding basic plugins.
 mkdir plugins
 cd plugins
-curl -O https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.5.1/PAPER/ViaVersion-5.5.1.jar
-curl -O https://hangarcdn.papermc.io/plugins/ViaVersion/ViaBackwards/versions/5.5.1/PAPER/ViaBackwards-5.5.1.jar
-curl -O https://hangarcdn.papermc.io/plugins/ViaVersion/ViaRewind/versions/4.0.11/PAPER/ViaRewind-4.0.11.jar
+curl -O https://cdn.modrinth.com/data/P1OZGk5p/versions/hIC4C8re/ViaVersion-5.11.0-SNAPSHOT.jar
+curl -O https://cdn.modrinth.com/data/NpvuJQoq/versions/uf4Mgp8c/ViaBackwards-5.11.0-SNAPSHOT.jar
+curl -O https://cdn.modrinth.com/data/TbHIxhx5/versions/ePgt0AZz/ViaRewind-4.1.3-SNAPSHOT.jar
 echo Creating runserver.sh
  cd ..
 curl -o runserver.sh https://raw.githubusercontent.com/Lucas6228/Minecraft-server-builder/refs/heads/main/runserver.sh
 chmod +x runserver.sh
 echo Your server is ready to run!
-echo Run the server with the runserver.sh file in the mcjavaserver directory. Use a terminal. Dont just double click the file.
+echo Run the server with the runserver.sh file in the PaperMC26.1.2 directory.
