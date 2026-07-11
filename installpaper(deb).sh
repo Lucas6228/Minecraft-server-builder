fb6a1b2f4904b5d8213be26ece80a688adc7539b
Lucas6228 <146557843+Lucas6228@users.noreply.github.com>
2026-07-11 17:10:56 -0400

    Update installpaper(deb).sh

--- installpaper(deb).sh
+++ installpaper(deb).sh
@@ -7,13 +7,6 @@ sudo apt install -y openjdk-21-jdk
 echo Making folder for server
 mkdir PaperMC26.1.2
 cd PaperMC26.1.2
-echo Downloading server jar
-curl -o server.jar https://fill-data.papermc.io/v1/objects/1d70b1dab9cf4a6de615209a536f3a45a2186240253c428213ce2188ab95e5f7/paper-26.1.2-74.jar
-echo Starting server for the first time to create files. If it errors. Ignore it
-java -Xms1G -Xmx1G -jar server.jar
-echo Accepting EULA
-echo For legal reasons. Please read the EULA.
-sed -i 's/eula=false/eula=true/' eula.txt
 
 echo Adding basic plugins.
 mkdir plugins
@@ -25,5 +18,13 @@ echo Creating runserver.sh
  cd ..
 curl -o runserver.sh https://raw.githubusercontent.com/Lucas6228/Minecraft-server-builder/refs/heads/main/runserver.sh
 chmod +x runserver.sh
+
+echo Downloading server jar
+curl -o server.jar https://fill-data.papermc.io/v1/objects/1d70b1dab9cf4a6de615209a536f3a45a2186240253c428213ce2188ab95e5f7/paper-26.1.2-74.jar
+echo Starting server for the first time to create files. If it errors. Ignore it
+java -Xms1G -Xmx1G -jar server.jar
+echo Accepting EULA
+echo For legal reasons. Please read the EULA.
+sed -i 's/eula=false/eula=true/' eula.txt
 echo Your server is ready to run!
 echo Run the server with the runserver.sh file in the PaperMC26.1.2 directory.
